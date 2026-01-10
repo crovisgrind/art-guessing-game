acf85319aa2b866bd3be41284ee0f99aeb4d72ee
Your Name <crovisgrind@gmail.com>
2026-01-10 11:26:42 -0300

    new artists

--- src/paintings.js
+++ src/paintings.js
@@ -70,6 +70,71 @@ export const paintings = [
     artist: "Turner",
     image: "/artworks/turner_the-fighting-temeraire.jpg",
     grid: 6
+  },
+  {
+    id: "the-ballet-class",
+    artist: "degas",
+    image: "/artworks/degas_the-ballet-class.jpg",
+    grid: 6
+  },
+  {
+    id: "montagne-sainte-victoire",
+    artist: "paul-cezanne",
+    image: "/artworks/paul-cezanne_montagne-sainte-victoire.jpg",
+    grid: 6
+  },
+  {
+    id: "fox-hill-upper-norwood",
+    artist: "pissarro",
+    image: "/artworks/pissarro_fox-hill-upper-norwood.jpg",
+    grid: 6
+  },
+   {
+    id: "creation-of-adam",
+    artist: "michelangelo",
+    image: "/artworks/michelangelo_creation-of-adam.jpg",
+    grid: 6
+  },
+  {
+    id: "vairumati",
+    artist: "paul-gauguin",
+    image: "/artworks/paul-gauguin_vairumati.jpg",
+    grid: 6
+  },
+  {
+    id: "le-moulin-de-la-galette",
+    artist: "renoir",
+    image: "/artworks/renoir_le-moulin-de-la-galette.jpg",
+    grid: 6
+  },
+{
+    id: "fugue",
+    artist: "kandinsky",
+    image: "/artworks/kandinsky_fugue.jpg",
+    grid: 6
+  },
+  {
+    id: "cristo-crucificado",
+    artist: "velazquez",
+    image: "/artworks/velazquez_cristo-crucificado.jpg",
+    grid: 6
+  },
+ {
+    id: "young-flautist",
+    artist: "manet",
+    image: "/artworks/manet_young-flautist.jpg",
+    grid: 6
+  },
+   {
+    id: "the-wedding-at-cana",
+    artist: "veronese",
+    image: "/artworks/veronese_the-wedding-at-cana.jpg",
+    grid: 6
+  },
+  {
+    id: "marchioness-of-la-solana",
+    artist: "goya",
+    image: "/artworks/goya_marchioness-of-la-solana.jpg",
+    grid: 6
   }
- 
 ]
